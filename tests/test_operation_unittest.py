29f7db4e62ee2d31d2bc184cca2a7ed80999497b
veniamin <venya957@gmail.com>
2024-01-31 16:28:01 +0300

    fixed

--- tests/test_operation_unittest.py
+++ tests/test_operation_unittest.py
@@ -1,22 +1,22 @@
 import unittest
 
-from tests.test_operation import operations
+from tests.test_operation import operation
 
-from utils.utils_operation import operation, operation_executed, last_five_operations, parse_operations_data
+from utils.utils_operation import load_operations, operation_executed, last_five_operations, parse_operations_data
 
 
 class TestOperations(unittest.TestCase):
     def test_operation(self):
-        assert operation(operations)
+        assert load_operations(operation)
 
     def test_operation_executed(self):
-        assert operation_executed(operations)
+        assert operation_executed(operation)
 
     def test_last_five_operations(self):
-        assert last_five_operations(operations)
+        assert last_five_operations(operation)
 
     def test_parse_operations_data(self):
-        assert parse_operations_data(operations)
+        assert parse_operations_data(operation)
 
 
 if __name__ == '__main__':
--- utils/utils_operation.py
+++ utils/utils_operation.py
@@ -1,25 +1,29 @@
-from utils.load_json import load_operations
+import json
 
 
-def operation(filename):
-    '''Чтение файла'''
-    operations = load_operations('../operations.json')
-    return operations
+def load_operations(filename):
+    """
+       Загружает список операций из файла
+
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
 
 
-def operation_executed(data):
+
+def operation_executed(filename):
     '''Поиск одобренных операций'''
     operation_executed_ = []
-    operation_equail = operation('../operations.json')
+    operation_equail = load_operations(filename)
     for i in operation_equail:
         if i.get("state") == "EXECUTED":
             operation_executed_.append(i)
     return operation_executed_
 
 
-def last_five_operations(operation_execute):
+def last_five_operations(operation_executed_):
     '''Вывод последних 5-ти опреаций'''
-    sorted_operations = sorted(operation_execute, key=lambda x: x["date"], reverse=True)
+    sorted_operations = sorted(operation_executed_, key=lambda x: x["date"], reverse=True)
     last_five = sorted_operations[0:5]
     return last_five
 
