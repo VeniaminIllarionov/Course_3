7e4dc3ccf6afa775c1ab321a9808633daa1cd5de
veniamin <venya957@gmail.com>
2024-01-31 21:05:49 +0300

    Tests

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -7,7 +7,9 @@ operation = 'tests.operations.json'
 def test_operation_executed():
     assert len(operation_executed(operation)) == 3
     print(operation_executed(operation))
-
+    assert operation_executed(operation) == [{'state': 'EXECUTED'},
+                                             {'state': 'EXECUTED'},
+                                             {'state': 'EXECUTED'}]
 
 
 def test_last_five_operations():
@@ -15,7 +17,7 @@ def test_last_five_operations():
     print(dates)
     assert dates == ['2019-11-19T09:22:25.899614',
                      '2019-11-05T12:04:13.781725',
-                     '2019-10-30Tgi01:49:52.939296',
+                     '2019-10-30T01:49:52.939296',
                      '2019-08-08T21:58:06.688541',
                      '2019-06-14T19:37:49.044089']
 
@@ -23,4 +25,3 @@ def test_last_five_operations():
 def test_parse_operations_data():
     assert parse_operations_data(operations)
     print(parse_operations_data(operations))
-
